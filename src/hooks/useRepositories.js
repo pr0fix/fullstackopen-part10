0f1bdd932ebaf25d7db4a0caf10d3dd072d254b3
pr0fix <bgx152@myy.haaga-helia.fi>
2024-09-27 21:16:43 +0300

    osa10 tehtavat 10.11 & 10.12

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,17 @@
+import { useQuery } from "@apollo/client";
+import { GET_REPOSITORIES } from "../graphql/queries";
+
+
+const useRepositories = () => {
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
+
+  const repositories = data?.repositories?.edges
+    ? data.repositories.edges.map((edge) => edge.node)
+    : [];
+
+  return { repositories, error, loading };
+};
+
+export default useRepositories;
---
+++ src/utils/apolloClient.js
@@ -0,0 +1,10 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import Constants from "expo-constants";
+const createApolloClient = () => {
+  return new ApolloClient({
+    uri: Constants.expoConfig.extra.uri,
+    cache: new InMemoryCache(),
+  });
+};
+
+export default createApolloClient;
